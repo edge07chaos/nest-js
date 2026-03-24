a16494c63484e72b8233be6b2d4d8d65014caeda
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 23:14:33 +0100

    lint

--- src/common/pipes/zod-validation.pipe.ts
+++ src/common/pipes/zod-validation.pipe.ts
@@ -10,16 +10,15 @@ export class ZodValidationPipe implements PipeTransform {
 
   transform(value: unknown, metadata: ArgumentMetadata) {
     try {
-      console.log("metadata.type: ", metadata.type);
-      
+      console.log('metadata.type: ', metadata.type);
+
       const parsedValue = this.schema.parse(value);
       return parsedValue;
     } catch (error) {
-      if (error instanceof ZodError)
-      {
+      if (error instanceof ZodError) {
         // throw new BadRequestException(z.prettifyError(error));
         // throw new BadRequestException(z.treeifyError(error))
-        throw new BadRequestException(z.flattenError(error))
+        throw new BadRequestException(z.flattenError(error));
       }
       throw new BadRequestException('Validation failed');
     }
