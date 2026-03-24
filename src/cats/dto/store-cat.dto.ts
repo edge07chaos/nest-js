8f0e117c75600461e11ef37a47ba5dc3d633ca22
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 17:52:19 +0100

    create store-cat-dto from a zod schema

--- src/cats/dto/store-cat.dto.ts
+++ src/cats/dto/store-cat.dto.ts
@@ -1,6 +1,28 @@
-export class StoreCatDTO {
-  id: string;
-  name: string;
-  age: number;
-  breed: string;
-}
+import z from 'zod';
+// import { IsInt, IsString, IsUUID, isUUID } from "class-validator";
+
+
+// export class StoreCatDTO {
+//   @IsUUID()
+//   id: string;
+
+//   @IsString()
+//   name: string;
+
+//   @IsInt()
+//   age: number;
+
+//   @IsString()
+//   breed: string;
+//}
+
+export const storeCatSchema = z
+  .object({
+    id: z.uuid(),
+    name: z.string(),
+    age: z.number(),
+    breed: z.string(),
+  })
+  .required();
+
+export type StoreCatDTO = z.infer<typeof storeCatSchema>;
