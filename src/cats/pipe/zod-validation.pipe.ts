314c2b41773b1de58a3bc0f1f610fabb83e245cf
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 17:51:49 +0100

    implements zod validation pipe which parse a schema

---
+++ src/cats/pipe/zod-validation.pipe.ts
@@ -0,0 +1,28 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+  HttpStatus,
+} from '@nestjs/common';
+import z, { ZodType, ZodError } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodType) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      console.log("metadata.type: ", metadata.type);
+      
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      if (error instanceof ZodError)
+      {
+        // throw new BadRequestException(z.prettifyError(error));
+        // throw new BadRequestException(z.treeifyError(error))
+        throw new BadRequestException(z.flattenError(error))
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
