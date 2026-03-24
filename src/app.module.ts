b335fc5081f076751a45632c6570c0c52537d3d7
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 14:30:35 +0100

    apply logger middleware to cats controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/c
 import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddlewareClass } from './common/middlewares/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
@@ -10,6 +11,7 @@ import { LoggerMiddlewareClass } from './common/middlewares/logger.middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     // consumer.apply(LoggerMiddlewareClass).forRoutes('cats');
-    consumer.apply(LoggerMiddlewareClass).forRoutes({path: 'cats', method: RequestMethod.POST})
+    // consumer.apply(LoggerMiddlewareClass).forRoutes({path: 'cats', method: RequestMethod.POST})
+    consumer.apply(LoggerMiddlewareClass).forRoutes(CatsController);
   }
 }
