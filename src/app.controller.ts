5255e86ea9473d91c9129517fc5c74ef2b35cb61
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 14:19:30 +0100

    remove unused service in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
   @Get()
   status() {
     return {
-      message: "the serve if fuckin up...check your side instead"
-    }
+      message: 'the serve if fuckin up...check your side instead',
+    };
   }
 }
