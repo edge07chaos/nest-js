d0ad732bc4b4366bf945fa86c4b428b94dbe7260
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 17:49:23 +0100

    comment log info

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -4,17 +4,16 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('logger-middleware-class used');
-    console.log("req.host: ", req.host);
-    console.log("req.hostname: ", req.hostname);
-    console.log("req.ip: ", req.ip);
-    console.log("req.method: ", req.method);
-    console.log("req.body: ", req.body);
-    console.log("req.query: ", req.query);
-    console.log("req.params: ", req.params);
-    console.log("req.headers: ", req.headers);
-    console.log("req.route: ", req.route);
-    
+    // console.log('logger-middleware-class used');
+    // console.log('req.host: ', req.host);
+    // console.log('req.hostname: ', req.hostname);
+    // console.log('req.ip: ', req.ip);
+    // console.log('req.method: ', req.method);
+    // console.log('req.body: ', req.body);
+    // console.log('req.query: ', req.query);
+    // console.log('req.params: ', req.params);
+    // console.log('req.headers: ', req.headers);
+    // console.log('req.route: ', req.route);
     next();
   }
 }
