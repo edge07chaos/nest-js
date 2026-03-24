395f9419488b4a006d5d1059f14b9e054c349790
JsExpertCoder <misterjs247000@gmail.com>
2026-03-24 12:06:56 +0100

    linting

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,45 +1,57 @@
-import { Body, Controller, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
-import { CreateCatDTO } from './cats.dtos';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateCatDTO } from './cats.dto';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
-    index(@Query('name') name: string, @Query("age") age: number, @Query("breed") breed: string) {
-        return {
-            message: "listing all cats",
-            query: {
-                name, 
-                age,
-                breed
-            }
-        }
-    }
+  @Get()
+  index(
+    @Query('name') name: string,
+    @Query('age') age: number,
+    @Query('breed') breed: string,
+  ) {
+    return {
+      message: 'listing all cats',
+      query: {
+        name,
+        age,
+        breed,
+      },
+    };
+  }
 
-    @Post()
-    @HttpCode(201)
-    async create(@Body() createCatDTO: CreateCatDTO) {
-        console.log("creating cat:", {
-            name: createCatDTO.name,
-            age: createCatDTO.age,
-            breed: createCatDTO.breed
-        })
-        
-        return {
-            name: createCatDTO.name,
-            age: createCatDTO.age,
-            breed: createCatDTO.breed
-        }
-    }
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createCatDTO: CreateCatDTO) {
+    console.log('creating cat:', {
+      name: createCatDTO.name,
+      age: createCatDTO.age,
+      breed: createCatDTO.breed,
+    });
 
-    @Get(":id")
-    show(@Param("id") id: string) {
-        console.log("showing id:", id);
-        return {
-            data: {
-                name: "fake-name",
-                age: 678,
-                breed: "fake-breed"
-            }
-        }
-    }
+    return {
+      name: createCatDTO.name,
+      age: createCatDTO.age,
+      breed: createCatDTO.breed,
+    };
+  }
+
+  @Get(':id')
+  show(@Param('id') id: string) {
+    console.log('showing id:', id);
+    return {
+      data: {
+        name: 'fake-name',
+        age: 678,
+        breed: 'fake-breed',
+      },
+    };
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app
+    .listen(process.env.PORT ?? 3000)
+    .then(() => {
+      console.log('server is fuckin running baby!');
+    })
+    .catch((error) => {
+      console.log('server is crying, damm: ', error);
+    })
+    .finally(() => {
+      console.log('the end of the chain baby!');
+    });
 }
 bootstrap();
